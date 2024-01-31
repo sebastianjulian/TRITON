af8e2a5cc5733ca0a6b627c0251b6ad29ab31a14
Sebastian Maierhofer <sebastian@maierhofers.net>
2024-01-31 15:59:51 +0100

    sync

--- src/mpu6050_testscript.py
+++ src/mpu6050_testscript.py
@@ -1,6 +1,6 @@
 from mpu6050 import mpu6050
 
-sensor = mpu6050(0x68)  # Address may vary, run i2cdetect -y 1 to find the correct address
+sensor = mpu6050(0x77)  # Address may vary, run i2cdetect -y 1 to find the correct address
 
 accel_data = sensor.get_accel_data()
 gyro_data = sensor.get_gyro_data()
