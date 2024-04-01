23a8caec25f09ec5bdd5d2986ed6111ee1af621b
Sebastian Maierhofer <sebastian@maierhofers.net>
2024-04-01 19:36:03 +0200

    test

--- src/lora.py
+++ src/lora.py
@@ -1,5 +1,9 @@
 import serial
+print("BEFORE INIT")
 LORA_SERIAL_DEVICE = '/dev/serial0'
 LORA_SERIAL_BAUD_RATE = 9600
 lora = serial.Serial(LORA_SERIAL_DEVICE, baudrate=LORA_SERIAL_BAUD_RATE, timeout=3.0)
+print("AFTER INIT")
+print("BEFORE SEND")
 lora.write('test'.encode('utf-8'))
+print("AFTER SEND")
