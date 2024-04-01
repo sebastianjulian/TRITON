5e7f1a4ca661390a50dcb12d18cd8edcb71c9ffd
Sebastian Maierhofer <sebastian@maierhofers.net>
2024-04-01 19:24:11 +0200

    test

--- src/main.py
+++ src/main.py
@@ -44,7 +44,7 @@ data = np.zeros(12)
 is_lora_send_thread_running = False
 
 print("BEFORE LORA INIT")
-LORA_SERIAL_DEVICE = '\dev/serial0'
+LORA_SERIAL_DEVICE = '/dev/serial0'
 LORA_SERIAL_BAUD_RATE = 9600
 lora = serial.Serial(LORA_SERIAL_DEVICE, baudrate=LORA_SERIAL_BAUD_RATE, timeout=3.0)
 print("AFTER LORA INIT")
