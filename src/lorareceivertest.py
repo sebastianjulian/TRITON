4fb608511d710f9577e4dfd8981033619c5f68e3
sebastianjulian <sebastian@maierhofers.net>
2025-07-30 13:22:09 +0200

    sender and receiver test

---
+++ src/lorareceivertest.py
@@ -0,0 +1,10 @@
+import serial
+
+# Replace COM3 with your actual port, if different
+ser = serial.Serial('COM3', 9600, timeout=1)
+
+print("Listening for LoRa messages...")
+while True:
+    if ser.in_waiting:
+        data = ser.readline().decode(errors='ignore').strip()
+        print("Received:", data)
